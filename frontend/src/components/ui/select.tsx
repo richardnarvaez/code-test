da2ea743db57f158793f41bbd1d83cd73b529233
Richard Vinueza <richardvnarvaez@gmail.com>
2025-11-17 08:47:50 +0000

    Refactor select component import to use TypeScript type syntax for improved clarity

--- frontend/src/components/ui/select.tsx
+++ frontend/src/components/ui/select.tsx
@@ -1,4 +1,4 @@
-import { SelectHTMLAttributes, forwardRef } from "react"
+import { type SelectHTMLAttributes, forwardRef } from "react"
 import { cn } from "../../lib/utils"
 
 const Select = forwardRef<HTMLSelectElement, SelectHTMLAttributes<HTMLSelectElement>>(
